9ac84526b941cb352caa11d2a4e47085f59628dd
kama <kamilarmatys@g.pl>
2015-01-30 01:41:25 +0100

    Add swap colorspace bgr to rgb and add static values

--- gffFormat/class/ColorSettings.h
+++ gffFormat/class/ColorSettings.h
@@ -2,15 +2,33 @@
  * colorSettings.h
  *
  *  Created on: 16 sty 2015
- *      Author: kama
+ *  Author: team15
  */
 
 #ifndef COLORSETTINGS_H_
 #define COLORSETTINGS_H_
 
-class colorSettings {
+#include<algorithm>
+
+class ColorSettings {
 public:
-	colorSettings();
+	static const short RGB = 0;
+	static const short BGR = 1;
+	static const short HSL = 2;
+	static const short HSV = 3;
+	static const short YUV = 4;
+
+	ColorSettings(){
+
+	}
+
+	static std::vector<unsigned char> swapRedBlue(std::vector<unsigned char> bytes){
+		int length = bytes.size();
+		for(int i=0; i<length; i+=3){
+			std::swap(bytes[i],bytes[i+2]);
+		}
+		return bytes;
+	}
 };
 
 #endif /* COLORSETTINGS_H_ */
